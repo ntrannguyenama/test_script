036a609f4fd1db61ea66356568bd81e95fc71adb
NoriTN <ntrannguyen@amayas.fr>
2025-05-21 15:31:24 +0200

    Push selected files from Azure DevOps

---
+++ apps/scanbeton/mobile/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  appName: 'Scan Béton',
+  production: true,
+  backUrl: 'https://scanbetonama-dev-app.azurewebsites.net',
+  version: '0.3.42',
+  copyright: 'ScanBeton 2025',
+  homeUrl: '/main/settings',
+  logo: true,
+  organizationMode: 'mono',
+};
